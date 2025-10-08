5fbb1955d7cfdeebc798963b3116d5a281c0ae03
JuanesNavia08 <juannavia2008@gmail.com>
2025-10-07 23:17:55 -0500

    Feature: Termine todo lo grafico

---
+++ src/modelo/Horarios.java
@@ -0,0 +1,16 @@
+
+package modelo;
+
+/**
+ *
+ * @author Juanes
+ */
+public class Horarios {
+    public String nombreTutor;
+    public String estado;
+    public String dia;
+    public String hora;
+    public String lugarTutoria; 
+    
+    String ;
+}
