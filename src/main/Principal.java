721e7703ff6707ad53ba4194c602700fb9c393fd
Juan Cantillo <87282119+DAVI161718@users.noreply.github.com>
2025-10-11 20:12:04 -0500

    feat subiendo cambios pendientes para bajar soluciones de otras ramas

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
     public static void main(String[] args) {
         loginView login = new loginView();
         login.setVisible(true);
-        crearUsuario("12345678", "Juan Cantillo", "123", "Tutor", "1234567888");
+        crearUsuario("12345678", "Juan Cantillo", "123", "Admin", "1234567888");
     }
 
 }
