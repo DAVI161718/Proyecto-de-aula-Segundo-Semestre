fc739b15b2d590213ace0e551871dda933edda44
Juan Cantillo <87282119+DAVI161718@users.noreply.github.com>
2025-10-19 16:36:46 -0500

    feat: Agregue validadores para los cargos y semestres actuales del estudiante ademas coloque unos labels en el panel login

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -7,9 +7,9 @@ public class Principal {
     public static void main(String[] args) {
         loginView login = new loginView();
         login.setVisible(true);
-        crearUsuario("12345678", "Yeiny Alvares", "123", "Admin", "12345678","Activo","Semestre 1");
+        crearUsuario("12345678", "Yeiny Alvares", "123", "Admin", "12345678","Activo","");
         crearUsuario("123456789", "Juan Cantillo", "123", "Estudiante", "123456789","Inactivo","Semestre 2");
-        crearUsuario("1234567890", "Juan Navia", "123", "Tutor", "1234567890", "Inactivo","Semestre 3");
+        crearUsuario("1234567890", "Juan Navia", "123", "Tutor", "1234567890", "Inactivo","");
     }
 
 }
